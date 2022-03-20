d109c61c00f5fe3ddd8ee49992a0ac387635c1af
artgalla8 <artgalla8@gmail.com>
2022-03-20 18:19:01 +0000

    html finished and javascript done

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() { startPlayer(); }, false);
+let player;
+
+function startPlayer() 
+{
+ player = document.getElementById('video_player');
+ player.controls = false;
+}
+
+function playVideo()
+{
+ player.play();
+}
+
+function pauseVideo()
+{
+ player.pause();
+}
+
+function stopVideo() 
+{
+ player.pause();
+ player.currentTime = 0;
+}
+
+function changeVolume()
+{
+ player.volume = document.getElementById("change_vol").value;
+}
